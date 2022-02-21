3e1292e1b529c8f301e36866c2c2a8a1b6579c1b
Tyler Young <tyler.young@stirlinglabs.com>
2022-02-21 17:37:26 -0500

    implement alloc headers, protected frees

--- sa.c
+++ sa.c
@@ -1,9 +1,26 @@
 #include "common.h"
 #include "sa.h"
 
+#if INTPTR_MAX == INT64_MAX
+static intptr_t sa_alloc_cookie = 0x7259813133E9FAA6;
+#elif INTPTR_MAX == INT32_MAX
+static intptr_t sa_alloc_cookie = 0x33E9FAA6;
+#else
+#error Not yet implemented
+#endif
+
+typedef struct sa_alloc_header {
+    intptr_t cookie;
+    sa_free_t *free;
+} sa_alloc_header_t;
+
+alignas(sa_mm_fns_align)
+static sa_mm_fns_t sa_mm_fns;
+
 static void *default_sa_alloc(size_t size) {
+    // assume size will always be greater than sizeof(sa_alloc_header_t) 
     void *p = malloc(size);
-    memset(p, 0, size);
+    memset(&((sa_alloc_header_t *)p)[1], 0, size - sizeof(sa_alloc_header_t));
     return p;
 }
 
@@ -26,13 +43,25 @@ sa_mm_fns_t xch_sa_mm_fns(sa_mm_fns_t *p) {
 }
 
 INLINE void *sa_alloc(size_t size) {
-    return (*sa_mm_fns.alloc)(size);
+    if (size == 0) return NULL;
+    // TODO: ensure atomic local copy of sa_mm_fns
+    sa_mm_fns_t mm = sa_mm_fns;
+    void * p = (*mm.alloc)(size + sizeof(sa_alloc_header_t));
+    sa_alloc_header_t * h = p;
+    h->cookie = sa_alloc_cookie;
+    h->free = mm.free;
+    void * d = &h[1];
+    return d;
 }
 
 void sa_free(const void *p) {
-    if (p != NULL) {
-        (*sa_mm_fns.free)(p);
-    }
+    if (p == NULL)
+        return;
+    sa_alloc_header_t *ph = &((sa_alloc_header_t *)p)[-1];
+    sa_alloc_header_t h = *ph; // local copy
+    if (h.cookie != sa_alloc_cookie)
+        return;
+    h.free(ph);
 }
 
 SA_BOOL sa_is_unspec(struct sockaddr *sa) {
