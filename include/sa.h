58fe4f5726aae495e6950d85fd46f04be6c2f776
Robert J Spencer <rj@game.net.au>
2022-03-11 05:27:04 +0800

    fix doc formatting

--- include/sa.h
+++ include/sa.h
@@ -18,7 +18,7 @@
 
 /**
  * @brief SA_BOOL booleans are non-zero integers for true, zero for false.
- * */
+ */
 #ifdef DOXYGEN
 #define SA_EXPORT extern
 #else
@@ -35,19 +35,19 @@ extern "C" {
 
 /**
  * @brief SA_BOOL booleans are non-zero integers for true, zero for false.
- * */
+ */
 #define SA_BOOL int
 
 /**
  * @brief A memory allocation function that can be used by this implementation.
  * @param[in] size A size in bytes to allocate.
- * */
+ */
 typedef void *sa_alloc_t(size_t size);
 
 /**
  * @brief A memory freeing function that can be used by this implementation.
  * @param[in] p A pointer to memory to be freed.
- * */
+ */
 typedef void sa_free_t(const void *p);
 
 /**
@@ -57,7 +57,7 @@ typedef void sa_free_t(const void *p);
 
 /**
  * @brief A matched pair of memory allocation and free functions.
- * */
+ */
 typedef struct sa_mm_fns {
     alignas(sa_mm_fns_align) sa_alloc_t * alloc;
     sa_free_t * free;
@@ -73,28 +73,28 @@ static_assert(alignof(sa_mm_fns_t) == sa_mm_fns_align,
  * @brief Identifies socket addresses that are AF_UNSPEC.
  * @param[in] sa Pointer to a socket address.
  * @return non-zero if the sa is AF_UNSPEC.
- * */
+ */
 SA_EXPORT SA_BOOL sa_is_unspec(struct sockaddr *sa);
 
 /**
  * @brief Identifies socket addresses that are AF_INET.
  * @param[in] sa Pointer to a socket address.
  * @return non-zero if the sa is AF_INET.
- * */
+ */
 SA_EXPORT SA_BOOL sa_is_ipv4(struct sockaddr *sa);
 
 /**
  * @brief Identifies socket addresses that are AF_INET6.
  * @param[in] sa Pointer to a socket address.
  * @return non-zero if the sa is AF_INET6.
- * */
+ */
 SA_EXPORT SA_BOOL sa_is_ipv6(struct sockaddr *sa);
 
 /**
  * @brief Gets the copyable size of the socket address.
  * @param[in] sa Pointer to a socket address.
  * @return Zero if unknown, otherwise the relevant copyable size.
- * */
+ */
 SA_EXPORT size_t sa_get_size(struct sockaddr *sa);
 
 /**
@@ -102,35 +102,35 @@ SA_EXPORT size_t sa_get_size(struct sockaddr *sa);
  * @attention This is provided for advanced use at the user's discretion.
  * @param[in] family A platform specific address family.
  * @return The size of a sockaddr_storage structure if unknown, otherwise the relevant copyable size.
- * */
+ */
 SA_EXPORT size_t sa_get_size_by_family(uint16_t family);
 
 /**
  * @brief Gets the copyable size of a sockaddr_storage structure.
  * @attention The return value may be treated as constant.
  * @return The copyable size.
- * */
+ */
 SA_EXPORT size_t sa_get_size_storage();
 
 /**
  * @brief Gets a generally safe copyable size of an unspecified family sockaddr structure.
  * @attention The return value may be treated as constant.
  * @return The copyable size.
- * */
+ */
 SA_EXPORT size_t sa_get_size_unspec();
 
 /**
  * @brief Gets the copyable size of a sockaddr_in structure.
  * @attention The return value may be treated as constant.
  * @return The copyable size.
- * */
+ */
 SA_EXPORT size_t sa_get_size_ipv4();
 
 /**
  * @brief Gets the copyable size of a sockaddr_in6 structure.
  * @attention The return value may be treated as constant.
  * @return The copyable size.
- * */
+ */
 SA_EXPORT size_t sa_get_size_ipv6();
 
 /**
@@ -138,7 +138,7 @@ SA_EXPORT size_t sa_get_size_ipv6();
  * @attention Caller should use sa_free(s) to clean up.
  * @param[in] sa Pointer to a socket address.
  * @return NULL if not possible, otherwise a pointer to a string.
- * */
+ */
 SA_EXPORT const char *sa_address_to_str(struct sockaddr *sa);
 
 /**
@@ -146,7 +146,7 @@ SA_EXPORT const char *sa_address_to_str(struct sockaddr *sa);
  * @attention Caller should use sa_free(sa) to clean up.
  * @param[in] port A port number to use.
  * @return NULL on failure, otherwise a pointer to the created socket address.
- * */
+ */
 SA_EXPORT struct sockaddr *sa_unspec(uint16_t port);
 
 
@@ -156,7 +156,7 @@ SA_EXPORT struct sockaddr *sa_unspec(uint16_t port);
  * @param[in] str A IPv4 string to parse.
  * @param[in] port A port number to use.
  * @return NULL on failure, otherwise a pointer to the created socket address.
- * */
+ */
 SA_EXPORT struct sockaddr *sa_ipv4(const char *str, uint16_t port);
 
 /**
@@ -165,7 +165,7 @@ SA_EXPORT struct sockaddr *sa_ipv4(const char *str, uint16_t port);
  * @param[in] address A IPv4 address in network byte order.
  * @param[in] port A port number to use.
  * @return NULL on failure, otherwise a pointer to the created socket address.
- * */
+ */
 SA_EXPORT struct sockaddr *sa_ipv4_bin(const uint8_t *address, uint16_t port);
 
 /**
@@ -174,7 +174,7 @@ SA_EXPORT struct sockaddr *sa_ipv4_bin(const uint8_t *address, uint16_t port);
  * @param[in] str A IPv6 string to parse.
  * @param[in] port A port number to use.
  * @return NULL on failure, otherwise a pointer to the created socket address.
- * */
+ */
 SA_EXPORT struct sockaddr *sa_ipv6(const char *str, uint16_t port);
 
 /**
@@ -183,14 +183,14 @@ SA_EXPORT struct sockaddr *sa_ipv6(const char *str, uint16_t port);
  * @param[in] address A IPv6 address in network byte order.
  * @param[in] port A port number to use.
  * @return NULL on failure, otherwise a pointer to the created socket address.
- * */
+ */
 SA_EXPORT struct sockaddr *sa_ipv6_bin(const uint8_t *address, uint16_t port);
 
 /**
  * @brief Gets the port of a socket address.
  * @param[in] sa Pointer to a socket address.
  * @return -1 on failure, otherwise a port number.
- * */
+ */
 SA_EXPORT int sa_get_port(struct sockaddr *sa);
 
 /**
@@ -198,7 +198,7 @@ SA_EXPORT int sa_get_port(struct sockaddr *sa);
  * @param[in] sa Pointer to a socket address.
  * @param[in] port A port number to use.
  * @return Non-zero on success.
- * */
+ */
 SA_EXPORT SA_BOOL sa_set_port(struct sockaddr *sa, uint16_t port);
 
 /**
@@ -206,7 +206,7 @@ SA_EXPORT SA_BOOL sa_set_port(struct sockaddr *sa, uint16_t port);
  * @attention Caller should use sa_free(s) to clean up.
  * @param[in] sa Pointer to a socket address.
  * @return -1 on failure, otherwise a scope name.
- * */
+ */
 SA_EXPORT const char *sa_get_scope(struct sockaddr *sa);
 
 /**
@@ -214,14 +214,14 @@ SA_EXPORT const char *sa_get_scope(struct sockaddr *sa);
  * @param[in] sa Pointer to a socket address.
  * @param[in] scope The name of a scope (network interface).
  * @return Non-zero on success.
- * */
+ */
 SA_EXPORT SA_BOOL sa_set_scope(struct sockaddr *sa, const char *scope);
 
 /**
  * @brief Gets the scope (network interface) index of a socket address.
  * @param[in] sa Pointer to a socket address.
  * @return Zero on failure or no scope, otherwise a valid scope index.
- * */
+ */
 SA_EXPORT uint32_t sa_get_scope_index(struct sockaddr *sa);
 
 /**
@@ -229,7 +229,7 @@ SA_EXPORT uint32_t sa_get_scope_index(struct sockaddr *sa);
  * @param[in] sa Pointer to a socket address.
  * @param[in] scope The index of a scope (network interface).
  * @return Non-zero on success.
- * */
+ */
 SA_EXPORT SA_BOOL sa_set_scope_index(struct sockaddr *sa, uint32_t scope);
 
 /**
@@ -238,7 +238,7 @@ SA_EXPORT SA_BOOL sa_set_scope_index(struct sockaddr *sa, uint32_t scope);
  * @param[in] sa Pointer to a socket address.
  * @param[in] family A platform specific address family value to specify.
  * @return Non-zero on success.
- * */
+ */
 SA_EXPORT SA_BOOL sa_set_family(struct sockaddr *sa, int family);
 
 /**
@@ -246,48 +246,48 @@ SA_EXPORT SA_BOOL sa_set_family(struct sockaddr *sa, int family);
  * @attention This is provided for advanced use at the user's discretion.
  * @param[in] sa Pointer to a socket address.
  * @return -1 on failure, otherwise the platform specific address family value.
- * */
+ */
 SA_EXPORT int sa_get_family(const struct sockaddr *sa);
 
 /**
  * @brief Gets the platform specific address family of an unspecified (AF_UNSPEC) socket address.
  * @attention This is provided for advanced use at the user's discretion.
  * @return -1 on failure, otherwise the platform specific address family value.
- * */
+ */
 SA_EXPORT int sa_get_family_unspec();
 
 /**
  * @brief Gets the platform specific address family of an IPv4 (AF_INET) socket address.
  * @attention This is provided for advanced use at the user's discretion.
  * @return -1 on failure, otherwise the platform specific address family value.
- * */
+ */
 SA_EXPORT int sa_get_family_ipv4();
 
 /**
  * @brief Gets the platform specific address family of an IPv6 (AF_INET6) socket address.
  * @attention This is provided for advanced use at the user's discretion.
  * @return -1 on failure, otherwise the platform specific address family value.
- * */
+ */
 SA_EXPORT int sa_get_family_ipv6();
 
 /**
  * @brief Detects if the socket address structure has a length field.
  * @attention This is provided for advanced use at the user's discretion.
  * @return Non-zero when true.
- * */
+ */
 SA_EXPORT SA_BOOL sa_has_len_field(void);
 
 /**
  * @brief Detects the address family field offset.
  * @attention This is provided for advanced use at the user's discretion.
  * @return The offset of the address family field in the socket address structure.
- * */
+ */
 SA_EXPORT ptrdiff_t sa_family_offset(void);
 
 /**
  * @brief Frees anything allocated by this library.
  * @param[in] p A pointer to something allocated by this library.
- * */
+ */
 SA_EXPORT void sa_free(const void *p);
 
 /**
@@ -295,7 +295,7 @@ SA_EXPORT void sa_free(const void *p);
  * @param[in] sa Pointer to a socket address.
  * @param[in] offset An offset into the address.
  * @return -1 on failure, otherwise the value of the read byte.
- * */
+ */
 SA_EXPORT int sa_get_address_byte(struct sockaddr *sa, size_t offset);
 
 /**
@@ -304,7 +304,7 @@ SA_EXPORT int sa_get_address_byte(struct sockaddr *sa, size_t offset);
  * @param[in] offset An offset into the address.
  * @param[in] value The byte value to write.
  * @return Non-zero on success.
- * */
+ */
 SA_EXPORT SA_BOOL sa_set_address_byte(struct sockaddr *sa, size_t offset, uint8_t value);
 
 /**
@@ -312,7 +312,7 @@ SA_EXPORT SA_BOOL sa_set_address_byte(struct sockaddr *sa, size_t offset, uint8_
  * @param[in] sa Pointer to a socket address.
  * @param[in] size Reports the count of bytes that make up the memory region.
  * @return Null on failure, otherwise a pointer to the address bytes of a socket address.
- * */
+ */
 SA_EXPORT const uint8_t* sa_address_bytes(struct sockaddr *sa, size_t* size);
 
 
@@ -320,14 +320,14 @@ SA_EXPORT const uint8_t* sa_address_bytes(struct sockaddr *sa, size_t* size);
  * @brief Gets the name of a scope (network interface) index.
  * @param[in] scope The index of a scope (network interface).
  * @return The name of the scope (network interface).
- * */
+ */
 SA_EXPORT const char *sa_scope_get_name(uint16_t scope);
 
 /**
  * @brief Finds the index for the name of a scope (network interface).
  * @param[in] scope The name of a scope (network interface).
  * @return The index of the scope (network interface).
- * */
+ */
 SA_EXPORT uint16_t sa_scope_get_index(const char *scope);
 
 /**
@@ -337,7 +337,7 @@ SA_EXPORT uint16_t sa_scope_get_index(const char *scope);
  * or if the parameter is NULL, gets the current memory management functions.
  * @param[in] p Null or a pointer to the new memory management functions.
  * @return The previous or current memory management functions.
- * */
+ */
 SA_EXPORT sa_mm_fns_t xch_sa_mm_fns(sa_mm_fns_t *p);
 
 #ifdef __cplusplus
